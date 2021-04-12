f965dc59f48ad4d12e423fead99747fed21c74a7
Michael <72650525+michael-ra@users.noreply.github.com>
2021-04-13 01:36:01 +0200

    Removed unused test / debug function

--- Assets/IKSnap.cs
+++ Assets/IKSnap.cs
@@ -321,17 +321,4 @@ public class IKSnap : MonoBehaviour
             anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
         }
     }
-
-
-    void DrawCircle(Vector3 center, float radius, Color color)
-    {
-        Vector3 prevPos = center + new Vector3(radius, 0, 0);
-        for (int i = 0; i < 30; i++)
-        {
-            float angle = (float)(i + 1) / 30.0f * Mathf.PI * 2.0f;
-            Vector3 newPos = center + new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
-            Debug.DrawLine(prevPos, newPos, color);
-            prevPos = newPos;
-        }
-    }
 }
